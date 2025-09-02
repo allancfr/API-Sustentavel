7523d32b98e5ac46b83a654e0dafc59e3c266b3a
Allan Cezar Fuck Rutes <allan_cfr@hotmail.com>
2025-09-02 15:34:22 -0300

    Initial commit

---
+++ src/test/java/br/com/futuro/api_sustentavel/ApiSustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futuro.api_sustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
